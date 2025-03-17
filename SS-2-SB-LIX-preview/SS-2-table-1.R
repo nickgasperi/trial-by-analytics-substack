e472dd6f2a7febb28c859e97a8c4cc446306d272
Nick Gasperi <nickgasperi5@gmail.com>
2025-03-16 23:46:10 -0500

    Update SS-2-table-1.R

--- SS-2-SB-LIX-preview/SS-2-table-1.R
+++ SS-2-SB-LIX-preview/SS-2-table-1.R
@@ -1,11 +1,9 @@
 # load packages
 library(tidyverse)
-library(dplyr)
 library(nflfastR)
 library(nflplotR)
 library(nflreadr)
 library(gt)
-library(webshot2)
 library(data.table)
 library(gtExtras)
 
