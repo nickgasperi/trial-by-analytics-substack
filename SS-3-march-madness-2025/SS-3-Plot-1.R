a6ae7bbf0d259992066d0e11c5b965555e106b9c
Nick Gasperi <nickgasperi5@gmail.com>
2025-04-03 12:55:54 -0500

    Create SS-3-Plot-1.R

---
+++ SS-3-march-madness-2025/SS-3-Plot-1.R
@@ -0,0 +1,49 @@
+library(tidyverse)        # data wrangling
+library(readxl)           # to import data
+library(ggimage)
+
+# convert dataframe to tibble
+kenbart1 = as_tibble(kenbart)
+
+# select only 2025 tournament
+kenbart2025 = kenbart1 %>%
+  filter(YEAR == 2025)
+
+# insert blank column where we can add logos
+kenbart2025[, "logo"] = NA
+
+# insert team logos for the four 1 seeds by conditionally replacing values
+kenbart2025$logo[kenbart2025$TEAM == "Auburn"] = "C:/Users/Nick Gasperi/Downloads/auburn-logo.png"
+kenbart2025$logo[kenbart2025$TEAM == "Florida"] = "C:/Users/Nick Gasperi/Downloads/florida-logo.png"
+kenbart2025$logo[kenbart2025$TEAM == "Houston"] = "C:/Users/Nick Gasperi/Downloads/houston-logo.png"
+kenbart2025$logo[kenbart2025$TEAM == "Duke"] = "C:/Users/Nick Gasperi/Downloads/duke-logo.png"
+
+# plot
+power1 = kenbart2025 %>%
+  mutate(pointcolor2 = ifelse(SEED > 1, "darkgrey", "white")) %>%
+  ggplot(aes(x = BARTHAG, y = WAB)) +
+  geom_smooth(method = "lm", se = FALSE,
+              color = "grey27") +
+  geom_point(aes(color = pointcolor2),
+             size = 3) +
+  geom_image(aes(image = kenbart2025$logo),
+             size = 0.06) +
+  scale_color_identity() +
+  labs(title = "Power Rating vs. Wins Above Bubble",
+       subtitle = "2025 March Madness",
+       caption = "By Nick Gasperi | @tbanalysis | Data @nishaanamin") +
+  theme_minimal() +
+  theme(legend.position = "none",
+        plot.background = element_rect(fill = "white"),
+        plot.title = element_text(hjust = 0.5, size = 24, face = "bold.italic"),
+        plot.subtitle = element_text(hjust = 0.5, size = 21, face = "bold.italic"),
+        plot.caption = element_text(size = 11),
+        axis.title = element_text(size = 15, face = "bold"),
+        axis.text = element_text(size = 15))
+
+# view plot
+power1
+
+# save the plot to the device's local files
+ggsave("SubSt3.1-barthag_wab.png",
+       width = 14, height = 10, dpi = "retina")
