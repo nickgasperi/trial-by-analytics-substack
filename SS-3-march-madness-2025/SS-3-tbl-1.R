3e973c2a9e56929b1efa1fe1a70d544163c24ee3
Nick Gasperi <nickgasperi5@gmail.com>
2025-04-04 00:18:16 -0500

    Create SS-3-tbl-1.R

---
+++ SS-3-march-madness-2025/SS-3-tbl-1.R
@@ -0,0 +1,29 @@
+library(tidyverse)  # data wrangling
+library(gt)         # create tables
+library(gtExtras)   # table formatting
+
+# load data
+kenbart1
+
+# select data for table
+kball = kenbart1 %>%
+  select(YEAR, TEAM, BARTHAG, WAB) %>%
+  filter(BARTHAG > 0.970 & WAB >= 9.5) %>%
+  arrange(-BARTHAG)
+
+# create table
+tbloneseed = gt(kball) %>%
+  gt_highlight_rows(rows = c(2, 4, 6, 11)) %>%
+  tab_footnote(footnote = md("*By Nick Gasperi | @tbanalysis | Data @nishaanamin*")) %>%
+  tab_options(footnotes.font.size = 9) %>%
+  gt_theme_pff() %>%
+  tab_style(style = cell_text(weight = "bold"),
+            locations = cells_body(columns = "YEAR",
+                                   rows = c(2, 4, 6, 11)))
+    
+# view table
+tbloneseed
+
+# save table to device's local files
+tbloneseed %>%
+  gtsave("Sub3.n-gt_barthag_wab.png")
