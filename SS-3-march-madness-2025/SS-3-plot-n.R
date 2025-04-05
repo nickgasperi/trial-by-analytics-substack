8f9fc792929bfa52176da1a6602ba363c5fb3ca5
Nick Gasperi <nickgasperi5@gmail.com>
2025-04-05 17:54:13 -0500

    Update Files

--- SS-3-march-madness-2025/SS-3-plot-n.R
+++ SS-3-march-madness-2025/SS-3-plot-n.R
@@ -6,10 +6,7 @@ kenbart1
 # create tibble 1
 kbprev = kenbart1 %>%
   select(SEED, YEAR, TEAM, WAB, BARTHAG, `BADJ EM`, `EFG%`) %>%
-  filter(YEAR == 2018 | YEAR == 2019 | YEAR == 2020 |
-         YEAR == 2021 | YEAR == 2022 | YEAR == 2023 |
-         YEAR == 2024,
-         SEED == 1) %>%
+  filter(YEAR < 2025, SEED == 1) %>%
   summarize(avgWAB = mean(WAB), avgBARTH = mean(BARTHAG),
             avgEFF = mean(`BADJ EM`), avgEFG = mean(`EFG%`)) %>%
   print(n = Inf)
@@ -32,14 +29,14 @@ kbcomp = bind_rows(kbprev, kbnow)
 
 # view new tibble
 kbcomp  # this tibble is formatted well for simply viewing, but we need to reshape it to make it usable in ggplot()
-        # since the tibble is very small, it is best to just manually rearrange our variables
+        # since the tibble is very small, just manually rearrange our variables
 # start a new tibble
 kbcomp2 = tibble(
   stat = c("avgWAB", "avgBARTH", "avgEFF", "avgEFG",
            "avgWAB", "avgBARTH", "avgEFF", "avgEFG"),
-  value = c(9.23, 0.957, 29.0, 55.1,
+  value = c(9.38, 0.958, 28.8, 54.2,
             11.2, 0.978, 35.8, 55.2) ,
-  seasons = c("'18-'24", "'18-'24", "'18-'24", "'18-'24",
+  seasons = c("'08-'24", "'08-'24", "'08-'24", "'08-'24",
               "'25", "'25", "'25", "'25")
   )
 
