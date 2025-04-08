2558e0c5d5f896e1de554877591ac795f794f8be
Nick Gasperi <nickgasperi5@gmail.com>
2025-04-07 19:42:27 -0500

    Create and Rename Files

--- SS-3-march-madness-2025/SS-3-plot-1.R
+++ SS-3-march-madness-2025/SS-3-plot-1.R
@@ -1,58 +1,42 @@
 # load packages
 library(tidyverse)    # data wrangling
 library(readxl)       # to import data
-library(ggrepel)
 
 # load data
 kenbart1
 
-# wrangle data into new tibble with only national champions since 2008 and the 2025 final 4 teams
-kbchamps = kenbart1 %>%
-  filter(ROUND == 1 | YEAR == 2025 & SEED == 1) %>%
-  print(n = Inf)
+# define top 5 conferences
+topconf = c("B12", "SEC", "ACC", "B10", "BE")
 
-# insert blank column where we can add logos
-kbchamps[, "logo"] = NA
-
-# insert team logos for the four 1 seeds by conditionally replacing values
-kbchamps$logo[kbchamps$TEAM == "Auburn" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/auburn-logo.png"
-kbchamps$logo[kbchamps$TEAM == "Florida" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/florida-logo.png"
-kbchamps$logo[kbchamps$TEAM == "Houston" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/houston-logo.png"
-kbchamps$logo[kbchamps$TEAM == "Duke" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/duke-logo.png"
-
-# insert column that combines year and team name
-kbchamps$teamyear = paste(kbchamps$TEAM, kbchamps$YEAR, sep = ", ")
-
-champsplot1 = kbchamps %>%
-  mutate(pointcolor4 = ifelse(YEAR == 2025, "white", "purple")) %>%
-  mutate(label4 = ifelse(YEAR == 2025, "", teamyear)) %>%
-  ggplot(aes(x = BARTHAG, y = `KADJ EM`)) +
-  geom_point(aes(color = pointcolor4),
-             size = 4) +
+# plot data
+wabexample = kenbart1 %>%
+  mutate(color5 = ifelse(CONF %in% topconf, "purple", "darkgrey")) %>%
+  ggplot(aes(x = TEAM, y = WAB)) +
+  geom_hline(yintercept = 0,
+             color = "red3",
+             linewidth = 1.5) +
+  geom_point(aes(color = color5)) +
   scale_color_identity() +
-  geom_text_repel(box.padding = 0.85,
-                  aes(label = label4,
-                      fontface = "bold"),
-                  size = 5.5) +
-  geom_image(aes(image = kbchamps$logo),
-             size = 0.065) +
-  labs(title = "Barttrovik Power Rating vs. Kenpom Adj. Efficiency",
-       subtitle = "National Champions 2008-2024",
+  labs(title = "Wins Against Bubble - NCAA Tournament Teams",
+       subtitle = "2008-2025 | purple = top 5 conf.",
        caption = "By Nick Gasperi | @tbanalysis | Data @nishaanamin") +
   theme_minimal() +
-  theme(legend.position = "none",
-        plot.background = element_rect(fill = "white"),
+  theme(plot.background = element_rect(fill = "white"),
         plot.title = element_text(hjust = 0.5,
-                                  face = "bold.italic", size = 22),
+                                  face = "bold.italic", size = 20),
         plot.subtitle = element_text(hjust = 0.5,
-                                     face = "bold.italic", size = 20),
-        plot.caption = element_text(size = 11),
-        axis.title = element_text(face = "bold", size = 16),
-        axis.text = element_text(size = 16))
+                                     face = "bold.italic",
+                                     size = 18),
+        plot.caption = element_text(size = 9),
+        axis.title.y = element_text(size = 16,
+                                  face = "bold"),
+        axis.text.y = element_text(size = 16),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank())
 
 # view plot
-champsplot1
+wabexample
 
-# save the plot to the device's local files
-ggsave("SubSt3.n-barthag_eff_champs.png",
-       width = 14, height = 10, dpi = "retina")
+# save plot to device's local files
+ggsave("SubSt3.n-wab_example.png",
+       width = 12, height = 9, dpi = "retina")
