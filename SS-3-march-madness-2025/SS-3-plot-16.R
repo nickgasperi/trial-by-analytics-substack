ba9b9b343687b3e71c6bb3f0bb4cd6b358dbcd8e
Nick Gasperi <nickgasperi5@gmail.com>
2025-05-02 23:37:59 -0500

    update file names

--- SS-3-march-madness-2025/SS-3-plot-16.R
+++ SS-3-march-madness-2025/SS-3-plot-16.R
@@ -1,85 +1,60 @@
 # load packages
-library(tidyverse)        # data wrangling
-library(readxl)           # to import data
-library(ggimage)          # replaces geom_point() for 2025 teams
+library(tidyverse)    # data wrangling
+library(readxl)       # to import data
+library(ggrepel)      # replaces geom_text()
+library(ggimage)      # replaces geom_text_repel() for 2025 data points
 
 # load data
 kenbart1
 
-# calculate Composite Rank
-kenbart1$comprank = (kenbart1$`KADJ EM RANK` + kenbart1$`BARTHAG RANK`)/2
-
-# add column to combine team and year
-kenbart1$SQUAD = paste(kenbart1$TEAM, kenbart1$YEAR, sep = ", ")
-
-# add 3 blank columns
-kenbart1[, "logo"] = NA
-kenbart1[, "colorpri"] = NA
-kenbart1[, "colorsec"] = NA
-
-# insert team logos for the four 1 seeds by conditionally replacing values
-kenbart1$logo[kenbart1$TEAM == "Auburn"] = "C:/Users/Nick Gasperi/Downloads/auburn-logo.png"
-kenbart1$logo[kenbart1$TEAM == "Florida"] = "C:/Users/Nick Gasperi/Downloads/florida-logo.png"
-kenbart1$logo[kenbart1$TEAM == "Houston"] = "C:/Users/Nick Gasperi/Downloads/houston-logo.png"
-kenbart1$logo[kenbart1$TEAM == "Duke"] = "C:/Users/Nick Gasperi/Downloads/duke-logo.png"
-
-# insert school primary and secondary colors
-# link to hex codes for all sports: 
-kenbart1$colorpri[kenbart1$TEAM == "Auburn" & kenbart1$YEAR == 2025] = "#0C2340"
-kenbart1$colorsec[kenbart1$TEAM == "Auburn" & kenbart1$YEAR == 2025] = "#E87722"
-kenbart1$colorpri[kenbart1$TEAM == "Florida" & kenbart1$YEAR == 2025] = "#0021A5"
-kenbart1$colorsec[kenbart1$TEAM == "Florida" & kenbart1$YEAR == 2025] = "#FA4616"
-kenbart1$colorpri[kenbart1$TEAM == "Houston" & kenbart1$YEAR == 2025] = "#C8102E"
-kenbart1$colorsec[kenbart1$TEAM == "Houston" & kenbart1$YEAR == 2025] = "#B2B4B2"
-kenbart1$colorpri[kenbart1$TEAM == "Duke" & kenbart1$YEAR == 2025] = "#003087"
-kenbart1$colorsec[kenbart1$TEAM == "Duke" & kenbart1$YEAR == 2025] = "#000000"
-
-# wrangle data into new tibble that includes only 2025 FF and prev. national champs
-kenbartchamps = kenbart1 %>%
-  filter(ROUND == 1 | SEED == 1 & YEAR == 2025) %>%
+# wrangle data into new tibble with only national champions since 2008 and the 2025 final 4 teams
+kbchamps = kenbart1 %>%
+  filter(ROUND == 1 | YEAR == 2025 & SEED == 1) %>%
   print(n = Inf)
 
-# plot data
-# after reordering, the y axis labels will get stiff - scale_y_discrete is used to make sure the important y-axis labels are incldued
-plot24 = kenbartchamps %>%
-  mutate(color24pri = ifelse(YEAR == 2025, colorpri, "grey90")) %>%
-  mutate(color24sec = ifelse(YEAR == 2025, colorsec, NA)) %>%
-  mutate(label24 = ifelse(YEAR < 2025, YEAR, "")) %>%
-  ggplot(aes(x = reorder(SQUAD, comprank), y = reorder(comprank, -comprank))) +
-  geom_bar(stat = "identity",
-           aes(fill = color24pri,
-               color = color24sec),
-           linewidth = 0.8) +
-  scale_fill_identity() +
+# insert blank column where we can add logos
+kbchamps[, "logo"] = NA
+
+# insert team logos for the four 1 seeds by conditionally replacing values
+kbchamps$logo[kbchamps$TEAM == "Auburn" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/auburn-logo.png"
+kbchamps$logo[kbchamps$TEAM == "Florida" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/florida-logo.png"
+kbchamps$logo[kbchamps$TEAM == "Houston" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/houston-logo.png"
+kbchamps$logo[kbchamps$TEAM == "Duke" & kbchamps$YEAR == 2025] = "C:/Users/Nick Gasperi/Downloads/duke-logo.png"
+
+# insert column that combines year and team name
+kbchamps$teamyear = paste(kbchamps$TEAM, kbchamps$YEAR, sep = ", ")
+
+# plot power rating against adjusted efficiency
+champsplot1 = kbchamps %>%
+  mutate(pointcolor4 = ifelse(YEAR == 2025, "white", "purple")) %>%
+  mutate(label4 = ifelse(YEAR == 2025, "", teamyear)) %>%
+  ggplot(aes(x = BARTHAG, y = `KADJ EM`)) +
+  geom_point(aes(color = pointcolor4),
+             size = 4) +
   scale_color_identity() +
-  geom_text(aes(label = label24),
-            fontface = "bold.italic",
-            size = 7) +
-  geom_image(aes(image = kenbartchamps$logo),
-             size = 0.075,
-             position = position_stack(vjust = 1.05)) +
-  geom_hline(yintercept = 23,
-             linetype = "dashed") +
-  scale_y_discrete(breaks = c(1,2,3,4,5,24)) +
-  labs(title = "Barttorvik + Kenpom Composite Power Rank",
-       subtitle = "National Champions | '08-'25 Tournaments",
-       caption = "By Nick Gasperi | @tbanalysis | Data @nishaanamin",
-       x = "TEAM", y = "COMPOSITE RANK") +
+  geom_text_repel(box.padding = 0.85,
+                  aes(label = label4,
+                      fontface = "bold"),
+                  size = 5.5) +
+  geom_image(aes(image = kbchamps$logo),
+             size = 0.065) +
+  labs(title = "Barttorvik Power Rating vs. Kenpom Adj. Efficiency",
+       subtitle = "'08-'24 National Champions",
+       caption = "By Nick Gasperi | @tbanalysis | Data @nishaanamin") +
   theme_minimal() +
-  theme(panel.grid = element_line(color = "white"),
+  theme(legend.position = "none",
         plot.background = element_rect(fill = "white"),
         plot.title = element_text(hjust = 0.5,
-                                  size = 24, face = "bold.italic"),
+                                  face = "bold.italic", size = 24),
         plot.subtitle = element_text(hjust = 0.5,
-                                     size = 22, face = "bold.italic"),
-        plot.caption = element_text(size = 12),
-        axis.title = element_text(size = 16, face = "bold"),
-        axis.text.y = element_text(size = 16),
-        axis.text.x = element_blank())
+                                     face = "bold.italic", size = 22),
+        plot.caption = element_text(size = 11),
+        axis.title = element_text(face = "bold", size = 16),
+        axis.text = element_text(size = 16))
 
 # view plot
-plot24
-    
+champsplot1
+
 # save the plot to the device's local files
-ggsave("SS3-plot16-comp-rating-champs.png",
+ggsave("SS3-plot16-barthag_eff_champs.png",
        width = 14, height = 10, dpi = "retina")
